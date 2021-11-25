ba2da23700b8194381840789212306f66314e81c
Abdelhady Nagi <62413027+AbdelhadyNagi@users.noreply.github.com>
2021-11-25 18:49:46 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+// jQuery Scripting
+$(function () {
+  "use strict";
+  const swiper = new Swiper(".swiper", {
+    // options here
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+});
+// *.....................................................................*
